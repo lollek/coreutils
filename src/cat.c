0dd3455b7fe492310c68b3d9e86b9db6c764d7d3
Olle K <olle.kvarnstroem@gmail.com>
2014-04-14 11:04:59 +0200

    added -s / --squeeze-blanks

--- src/cat.c
+++ src/cat.c
@@ -21,11 +21,12 @@ int usage(int status) {
   fprintf(status ? stderr : stdout,
       "Usage: %s [OPTIONS] [FILE]\n"
       "Concatenate FILE(s), or standard input, to standard output.\n\n"
-      "  -b, --number-nonblank     number nonempty output lines, overrides -n\n"
-      "  -E, --show-ends           display $ at end of each line\n"
-      "  -n, --number              number all output lines\n"
-      "      --help                display this help and exit\n"
-      "      --version             output version information and exit\n\n"
+      "  -b, --number-nonblank    number nonempty output lines, overrides -n\n"
+      "  -E, --show-ends          display $ at end of each line\n"
+      "  -n, --number             number all output lines\n"
+      "  -s, --squeeze-blank      suppress repeated empty output lines\n"
+      "      --help               display this help and exit\n"
+      "      --version            output version information and exit\n\n"
       "With no FILE, or when FILE is -, standard input is read\n"
       , progname);
   return version(status);
@@ -44,6 +45,7 @@ int print_file(const char *filename, int option_flags) {
 
   char buf[BUFLEN];
   static unsigned line_counter = 0;
+  static bool last_line_was_blank = false;
 
   bool number_lines = false;
   if (option_flags & 0x1) { number_lines = true; }
@@ -51,15 +53,23 @@ int print_file(const char *filename, int option_flags) {
   if (option_flags & 0x4) { number_lines = false; number_nonblanks = true; }
   bool show_ends = false;
   if (option_flags & 0x2) { show_ends = true; }
+  bool squeeze_blanks = false;
+  if (option_flags & 0x8) { squeeze_blanks = true; }
 
   /* With flags */
-  if (number_lines || number_nonblanks || show_ends) {
+  if (number_lines || number_nonblanks || show_ends || squeeze_blanks) {
     while (fgets(buf, BUFLEN, fd) != NULL) {
       const char *tmpbuf = buf;
       for (char *c = buf; *c != '\0'; ++c) {
         if (*c == '\n') {
           *c++ = '\0';
-          if (number_lines || (number_nonblanks && *tmpbuf != '\0')) {
+          bool line_is_blank = *tmpbuf == '\0';
+          if (squeeze_blanks && line_is_blank && last_line_was_blank) {
+            tmpbuf = c;
+            continue;
+          }
+          last_line_was_blank = line_is_blank;
+          if (number_lines || (number_nonblanks && !line_is_blank)) {
             printf("%6d\t", ++line_counter);
           }
           printf("%s", tmpbuf);
@@ -84,7 +94,6 @@ int print_file(const char *filename, int option_flags) {
       printf("%s", buf);
     }
   }
-
   return 0;
 }
 
@@ -96,12 +105,13 @@ int main(int argc, char **argv) {
     {"number-nonblank", no_argument, 0, 'b'},
     {"show-ends",       no_argument, 0, 'E'},
     {"number",          no_argument, 0, 'n'},
+    {"squeeze-blanks",  no_argument, 0, 's'},
     {"help",            no_argument, 0,  0},
     {"version",         no_argument, 0,  1}
   };
 
   while (1) {
-    int c = getopt_long(argc, argv, "bEn", long_options, &option_index);
+    int c = getopt_long(argc, argv, "bEns", long_options, &option_index);
     if (c == -1) {
       break;
     }
@@ -109,6 +119,7 @@ int main(int argc, char **argv) {
       case 'b': option_flags |= 0x4; break;
       case 'E': option_flags |= 0x2; break;
       case 'n': option_flags |= 0x1; break;
+      case 's': option_flags |= 0x8; break;
       case  0: return usage(0);
       case  1: return version(0);
       default:
