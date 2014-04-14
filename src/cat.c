16f6bf114788c39622a3a2929c1c13f8841580f9
Olle K <olle.kvarnstroem@gmail.com>
2014-04-14 10:04:27 +0200

    added -E/--show-ends

--- src/cat.c
+++ src/cat.c
@@ -21,6 +21,7 @@ int usage(int status) {
   fprintf(status ? stderr : stdout,
       "Usage: %s [OPTIONS] [FILE]\n"
       "Concatenate FILE(s), or standard input, to standard output.\n\n"
+      "  -E, --show-ends           display $ at end of each line\n"
       "  -n, --number              number all output lines\n"
       "      --help                display this help and exit\n"
       "      --version             output version information and exit\n\n"
@@ -42,27 +43,41 @@ int print_file(const char *filename, int option_flags) {
 
   char buf[BUFLEN];
   unsigned line_counter = 0;
-  bool number_lines = false;
-  if (option_flags & 0x1) {
-    number_lines = true;
-  }
 
-  while (fgets(buf, BUFLEN, fd) != NULL) {
-    const char *tmpbuf = buf;
-    if (number_lines) {
+  bool number_lines = false;
+  if (option_flags & 0x1) { number_lines = true; }
+  bool show_ends = false;
+  if (option_flags & 0x2) { show_ends = true; }
+
+  /* With flags */
+  if (number_lines || show_ends) {
+    while (fgets(buf, BUFLEN, fd) != NULL) {
+      const char *tmpbuf = buf;
       for (char *c = buf; *c != '\0'; ++c) {
         if (*c == '\n') {
           *c++ = '\0';
-          printf("%6d  ", ++line_counter);
-          printf("%s\n", tmpbuf);
+          if (number_lines) {
+            printf("%6d  ", ++line_counter);
+          }
+          printf("%s", tmpbuf);
+          if (show_ends) {
+            putchar('$');
+          }
+          putchar('\n');
           tmpbuf = c;
         }
       }
       if (*tmpbuf != '\0') {
-        printf("%6d  ", ++line_counter);
+        if (number_lines) {
+          printf("%6d  ", ++line_counter);
+        }
         printf("%s", tmpbuf);
       }
-    } else {
+    }
+
+  /* Without any flags */
+  } else {
+    while (fgets(buf, BUFLEN, fd) != NULL) {
       printf("%s", buf);
     }
   }
@@ -78,16 +93,18 @@ int main(int argc, char **argv) {
   while (1) {
     int option_index = 0;
     static struct option long_options[] = {
-      {"number", no_argument, 0, 'n'},
-      {"help",   no_argument, 0,  0},
-      {"version",no_argument, 0,  1}
+      {"show-ends", no_argument, 0, 'E'},
+      {"number",    no_argument, 0, 'n'},
+      {"help",      no_argument, 0,  0},
+      {"version",   no_argument, 0,  1}
     };
 
-    c = getopt_long(argc, argv, "n", long_options, &option_index);
+    c = getopt_long(argc, argv, "En", long_options, &option_index);
     if (c == -1) {
       break;
     }
     switch (c) {
+      case 'E': option_flags |= 0x2; break;
       case 'n': option_flags |= 0x1; break;
       case  0: return usage(0);
       case  1: return version(0);
